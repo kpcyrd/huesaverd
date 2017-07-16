633d9d5b531feacf2c30e10b44ba9f1e257152c0
kpcyrd <git@rxv.cc>
2017-07-17 00:31:18 +0200

    Expect display as first argument

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Turns your screen on and off based on hue light status. This requires homeassistant. Even though this was written for hue lights, you can query any enitity as long as it uses `"state": "on"`.
 
 ```
-RUST_LOG=huesaverd=info cargo run http://192.168.1.2:8123 light.over_the_rainbow
+RUST_LOG=huesaverd=info cargo run "$DISPLAY" http://192.168.1.2:8123 light.over_the_rainbow
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,55 @@ use std::thread;
 use std::time::Duration;
 use std::process::Command;
 
-fn run(bin: &str, args: Vec<&str>) {
-    Command::new(bin)
-         .args(args)
-         .status()
-         .expect("failed to execute process");
+struct Screensaver {
+    display: String,
 }
 
-fn turn_on() {
-    run("xset", vec!("dpms", "force", "on"));
-    run("xset", vec!("-dpms"));
-}
+impl Screensaver {
+    fn new(display: String) -> Screensaver {
+        Screensaver {
+            display,
+        }
+    }
 
-fn turn_off() {
-    run("xset", vec!("dpms", "force", "off"));
-    run("xset", vec!("+dpms"));
+    fn run(&self, bin: &str, args: Vec<&str>) {
+        Command::new(bin)
+             .args(args)
+             .env("DISPLAY", &self.display)
+             .status()
+             .expect("failed to execute process");
+    }
+
+    fn turn_on(&self) {
+        self.run("xset", vec!("dpms", "force", "on"));
+        self.run("xset", vec!("-dpms"));
+    }
+
+    fn turn_off(&self) {
+        self.run("xset", vec!("dpms", "force", "off"));
+        self.run("xset", vec!("+dpms"));
+    }
 }
 
 fn main() {
     env_logger::init().unwrap();
 
     let mut args = env::args().skip(1);
+    let display = args.next().unwrap();
     let url = args.next().unwrap();
     let entity = args.next().unwrap();
+
     let client = homeassistant::Client::new(url, None);
+    let saver = Screensaver::new(display);
 
     loop {
         match client.get_state(&entity) {
             Ok(response) => {
                 info!("{:?} is {:?}", entity, response.state);
                 if response.state == "on" {
-                    turn_on();
+                    saver.turn_on();
                 } else {
-                    turn_off();
+                    saver.turn_off();
                 }
             },
             Err(err) => println!("ERROR: {:?}", err),
